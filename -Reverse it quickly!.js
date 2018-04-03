34134c8b0cb682fd7ef2e2b64ccb8ead799694e9
Eric Bui <ericbui2@gmail.com>
2018-04-03 12:57:47 -0500

    04.03.2018 challenge (LEVEL 4 KYU)

---
+++ -Reverse it quickly!.js
@@ -0,0 +1,8 @@
+/*
+    LEVEL 4 KYU
+    Source: https://www.codewars.com/kata/reverse-it-quickly/train/javascript
+    Date: 04.03.2018
+*/
+
+// Reverse an array without using Array.reverse() and less than 28 characters :)
+weirdReverse=a=>a.sort(_=>1)
