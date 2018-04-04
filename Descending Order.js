d8ede013d7232e0bffe39e8f3fafaddbc41418d6
Eric Bui <ericbui2@gmail.com>
2018-04-04 11:16:53 -0500

    04.04.2018 challenge (LEVEL 7 KYU)

---
+++ Descending Order.js
@@ -0,0 +1,7 @@
+/*
+    LEVEL 7 KYU
+    Source: https://www.codewars.com/kata/descending-order/train/javascript
+    Date: 04.04.2018
+*/
+
+const descendingOrder = (n) => parseInt(n.toString().split("").sort().reverse().join(""));
