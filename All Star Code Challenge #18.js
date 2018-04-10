1fb6a0583a8a3f29fb9bb3317462a716a7ce37de
Eric Bui <ericbui2@gmail.com>
2018-04-10 16:46:40 -0500

    04.10.2018 challenge (LEVEL 8 KYU)

---
+++ All Star Code Challenge #18.js
@@ -0,0 +1,7 @@
+/*
+    LEVEL 8 KYU
+    Source: https://www.codewars.com/kata/all-star-code-challenge-number-18/train/javascript
+    Date: 04.10.2018
+*/
+
+const strCount = (str, letter) =>  str.split("").filter(x => x === letter).length;
