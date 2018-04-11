ec0201dab61d5335d2e61c7a99cbc39ff6187b73
Eric Bui <ericbui2@gmail.com>
2018-04-11 15:56:48 -0500

    04.11.2018 challenge (LEVEL 6 KYU)

---
+++ One line task: Square Every Digit.js
@@ -0,0 +1,7 @@
+/*
+    LEVEL 6 KYU
+    Source: https://www.codewars.com/kata/one-line-task-square-every-digit/train/javascript
+    Date: 04.11.2018
+*/
+
+sd=x=>Number(x.toString().split('').map(y=>y*y).join(''));
